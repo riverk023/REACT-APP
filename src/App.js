cff347b2d98060e24e6c2080e30348dffec9ca5a
riverk023 <riverk023@gmail.com>
2022-07-26 22:25:50 +0900

    [ep.34] 2022.07.26

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import TOC from "./Components/TOC";
-import Content from './Components/Content';
+import ReadContent from './Components/ReadContent';
+import CreateContent from './Components/CreateContent';
 import Subject from './Components/Subject';
+import Control from './Components/Control';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode:"read",
+      mode:"create",
       selected_content_id:2,
       subject:{title:'WEB', sub:'World wid web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -21,10 +24,11 @@ class App extends Component {
   }
 
   render() {
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === "read") {
       var i = 0;
       while(i < this.state.contents.length) {
@@ -36,7 +40,20 @@ class App extends Component {
         }
         i = i + 1;
       }
-      
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === "create") {
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // );
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        );
+        this.setState({
+          contents: _contents  
+        });
+      }.bind(this)}></CreateContent>
     }
     console.log('render', this);
 
@@ -55,15 +72,19 @@ class App extends Component {
         </Subject>
         <TOC
           onChangePage={function(id){
-            alert('hi');
             this.setState({
               mode:'read',
               selected_content_id:Number(id)
             })
           }.bind(this)}
           data={this.state.contents}></TOC>
-        <Content title={_title} desc={_desc}></Content>
-      </div>
+        <Control onChangeMode={function(_mode){
+            this.setState({
+              mode: _mode
+            })
+        }}></Control>
+        {_article}
+      </div> 
     );
   }
 }
